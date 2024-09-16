2cfec5486af9149dc7acaeee933654fa2f9e528f
naithar01 <eeung025@gmail.com>
2024-09-16 21:43:14 +0900

    refactor env config

--- config/config.go
+++ config/config.go
@@ -1,9 +1,23 @@
 package config
 
 import (
+	"log"
+
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv() error {
-	return godotenv.Load()
+func LoadEnv(filePaths ...string) error {
+	// 환경 변수 파일 경로가 주어지지 않으면 기본 `.env` 파일 로드
+	if len(filePaths) == 0 {
+		filePaths = append(filePaths, ".env")
+	}
+
+	// 환경 변수 로드 시도
+	if err := godotenv.Load(filePaths...); err != nil {
+		log.Printf("Error loading .env file: %v", err)
+		return err
+	}
+
+	log.Println(".env file loaded successfully")
+	return nil
 }
