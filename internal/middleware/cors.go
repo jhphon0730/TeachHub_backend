0a3be3f54e49f8bf869985b8a1d7309c0f824b0e
jhphon0730 <eeung025@gmail.com>
2024-09-21 00:15:13 +0900

    Change Middleware Directory Strcuture

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-type Middleware func(http.Handler) http.Handler
-
 // CORS 미들웨어
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -26,25 +24,4 @@ func CORS(next http.Handler) http.Handler {
 		// 다음 핸들러로 전달
 		next.ServeHTTP(w, r)
 	})
-}
-
-// 로깅 미들웨어
-func Logger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 요청을 로깅
-		log.Printf("Request: %s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		
-		// 다음 핸들러로 전달
-		next.ServeHTTP(w, r)
-	})
-}
-
-// 미들웨어 체인을 생성하는 함수
-func ChainMiddleware(xs ...Middleware) Middleware {
-	return func(next http.Handler) http.Handler {
-		for _, x := range xs {
-			next = x(next)
-		}
-		return next
-	}
 }
---
+++ internal/middleware/middleware.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+type Middleware func(http.Handler) http.Handler
+
+// 미들웨어 체인을 생성하는 함수
+func ChainMiddleware(xs ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		for _, x := range xs {
+			next = x(next)
+		}
+		return next
+	}
+}
