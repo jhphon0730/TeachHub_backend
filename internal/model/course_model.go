0f468d628b376ae6a892228b7d1d22b0d537afa3
jhphon0730 <eeung025@gmail.com>
2024-10-13 22:11:40 +0900

    refactor: Model 부리  - Course, Enrollment

--- internal/model/course_model.go
+++ internal/model/course_model.go
@@ -15,14 +15,6 @@ type Courses struct {
 	UpdatedAt 		time.Time `json:"updated_at"`
 }
 
-// 수강 테이블 ( courses_id<courses>, user_id<users> ) 
-type Enrollments struct {
-	ID 							int64    	`json:"id"`
-	Courses_id 			int64    	`json:"courses_id"` // 1 : N
-	Student_id 				int64   `json:"student_id"` 		// 1 : N
-	Enrollment_date time.Time `json:"enrollment_date"`
-}
-
 // ####################### Courses ####################### //
 func CreateCoursesTable() error {
 	createTableQuery := `
@@ -137,61 +129,5 @@ func FindCourseByEnrollments(enrollments []Enrollments) ([]Courses, error) {
 	}
 
 	return courses, nil
-}
-
-// ####################### Enrollments ####################### //
-func CreateEnrollmentsTable() error {
-	createTableQuery := `
-	CREATE TABLE IF NOT EXISTS enrollments (
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		courses_id INT NOT NULL,
-		student_id INT NOT NULL,
-		enrollment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-		FOREIGN KEY (courses_id) REFERENCES courses(id) ON DELETE CASCADE,
-		FOREIGN KEY (student_id) REFERENCES users(id) ON DELETE CASCADE
-	)
-	`
-	_, err := DB.Exec(createTableQuery)
-	return err
-}
-
-// 강의에 새로운 사용자(수강 학생) 추가 
-func InsertStudentEnrollment(course_id int64, student_id int64) (int64, error) {
-	query := "INSERT INTO enrollments (courses_id, student_id) VALUES (?, ?)"
-
-	result, err := DB.Exec(query, course_id, student_id)
-	if err != nil {
-		return 0, err
-	}
-
-	enrollmentsID, err := result.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-
-	return enrollmentsID, nil
-}
-
-// 학생이 속한 강의 ID들 조회 
-func FindEnrollmentsByStudentID(student_id int64) ([]Enrollments, error) {
-	query := "SELECT id, courses_id, student_id, enrollment_date FROM enrollments WHERE student_id = ?"
-
-	rows, err := DB.Query(query, student_id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var enrollments []Enrollments
-	for rows.Next() {
-		var enrollment Enrollments
-		if err := rows.Scan(&enrollment.ID, &enrollment.Courses_id, &enrollment.Student_id, &enrollment.Enrollment_date); err != nil {
-			return nil, err
-		}
-		enrollments = append(enrollments, enrollment)
-	}
-
-	return enrollments, nil
 }
 
