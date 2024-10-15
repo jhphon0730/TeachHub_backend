fc57bc38a7466a01b0da61a95dc655055b3752f2
jhphon0730 <eeung025@gmail.com>
2024-10-15 22:20:34 +0900

    refactor: 사용자 Username Field - Unique 추가

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID        int64     `json:"id"`
-	Username  string    `json:"username"`
+	Username  string    `json:"username"` // Unique
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	Bio       string    `json:"bio"`
@@ -19,7 +19,7 @@ func CreateUserTable() error {
 	createTableQuery := `
 	CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
-		username VARCHAR(50) NOT NULL,
+		username VARCHAR(50) NOT NULL UNIQUE,
 		email VARCHAR(50) NOT NULL,
 		password VARCHAR(255) NOT NULL,
 		bio VARCHAR(255) DEFAULT '',
